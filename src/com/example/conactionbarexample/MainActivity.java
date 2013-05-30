65570fdd53b9d4b5f07e95398ad2b9cdd385346b
Yu Lu <yulu8798@gmail.com>
2013-05-30 14:37:05 +0800

    add refresh and search button on the actionbar

--- src/com/example/conactionbarexample/MainActivity.java
+++ src/com/example/conactionbarexample/MainActivity.java
@@ -2,22 +2,31 @@ package com.example.conactionbarexample;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private MenuItem menuItem;
+	private ActionBar actionBar;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-		ActionBar actionBar = getActionBar();
-		actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE 
+		actionBar = getActionBar();
+		
+		actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME |ActionBar.DISPLAY_SHOW_TITLE
 				| ActionBar.DISPLAY_SHOW_CUSTOM);
 	}
 
@@ -33,11 +42,32 @@ public class MainActivity extends Activity {
 		switch(item.getItemId()){
 		case R.id.menu_load:
 			menuItem = item;
-			menuItem.setActionView(R.layout.actionbar_view);
+			menuItem.setActionView(R.layout.progressbar);
 			menuItem.expandActionView();
 			TestTask task = new TestTask();
 			task.execute("test");
 			break;
+			
+		case R.id.menu_search:
+			actionBar.setCustomView(R.layout.searchbar);
+
+			EditText search = (EditText)actionBar.getCustomView().findViewById(R.id.searchfield);
+			search.requestFocus();
+			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+			imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);	
+			
+			search.setOnEditorActionListener(new OnEditorActionListener(){
+
+				@Override
+				public boolean onEditorAction(TextView v, int actionId,
+						KeyEvent event) {
+					Toast.makeText(MainActivity.this, "search triggered", Toast.LENGTH_SHORT).show();					
+					actionBar.setCustomView(null);
+					return false;
+				}				
+			});			
+			break;
+			
 		default:
 			break;
 		}
